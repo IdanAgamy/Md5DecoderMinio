d6a63a8e500a1fd7e670f23cdc32780acb52f6bd
Idan Agamy <idanagamy@gmail.com>
2019-11-08 10:02:27 +0200

    Initial commit

---
+++ src/main/java/com/idanMd5DecoderMinion/Md5DecoderMinion/Md5DecoderMinionApplication.java
@@ -0,0 +1,13 @@
+package com.idanMd5DecoderMinion.Md5DecoderMinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5DecoderMinionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Md5DecoderMinionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idanMd5DecoderMinion/Md5DecoderMinion/Md5DecoderMinionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idanMd5DecoderMinion.Md5DecoderMinion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md5DecoderMinionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
